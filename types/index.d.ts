c01b310156a693c8dd1f3bf5e44ae5e1ee01bcc2
Samuel Menigat <samuel.menigat@gmail.com>
2021-07-05 10:46:38 +0200

    chore(ts): adds typescript types for package

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+declare namespace Cypress {
+  interface Chainable {
+    mhGetJimMode(): Chainable<boolean>;
+    mhSetJimMode(enabled : boolean): Chainable<Cypress.Response<any>>;
+    mhDeleteAll(): Chainable<Cypress.Response<any>>;
+    mhGetAllMails(): Chainable<mailhog.Item[]>;
+    mhFirst(): Chainable<mailhog.Item>;
+    mhGetMailsBySubject(subject : string): Chainable<mailhog.Item[]>;
+    mhGetMailsByRecipient(recipient : string): Chainable<mailhog.Item[]>;
+    mhGetMailsBySender(from : string): Chainable<mailhog.Item[]>;
+    mhGetSubject(): Chainable<string>;
+    mhGetBody(): Chainable<string>;
+    mhGetSender(): Chainable<string>;
+    mhGetRecipients(): Chainable<string[]>;
+    mhHasMailWithSubject(subject : string): Chainable;
+    mhHasMailFrom(from : string): Chainable;
+    mhHasMailTo(recipient: string): Chainable;
+  }
+}
