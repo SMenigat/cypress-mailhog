b0d78b27044094d7f3c7e7f70abd372243bdc3f4
danisawesome <danisawesome@users.noreply.github.com>
2022-07-31 00:18:24 -0700

    Lower default limit and add optional parameter

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,15 @@
 declare namespace Cypress {
   interface Chainable {
-    mhGetJimMode(auth: JSON): Chainable<boolean>;
-    mhSetJimMode(enabled : boolean, auth: JSON): Chainable<Cypress.Response<any>>;
-    mhDeleteAll(): Chainable<Cypress.Response<any>>;
-    mhGetAllMails(): Chainable<mailhog.Item[]>;
+    mhGetJimMode(auth?: JSON): Chainable<boolean>;
+    mhSetJimMode(enabled : boolean, auth?: JSON): Chainable<Cypress.Response<any>>;
+    mhDeleteAll(auth?: JSON): Chainable<Cypress.Response<any>>;
+    mhGetAllMails(auth?: JSON, limit?: number): Chainable<mailhog.Item[]>;
     mhFirst(): Chainable<mailhog.Item>;
     mhGetMailsBySubject(subject : string): Chainable<mailhog.Item[]>;
     mhGetMailsByRecipient(recipient : string): Chainable<mailhog.Item[]>;
     mhGetMailsBySender(from : string): Chainable<mailhog.Item[]>;
     mhGetSubject(): Chainable<string>;
-    mhGetBody(): Chainable<string>;
+    mhGetBody(): Chainablce<string>;
     mhGetSender(): Chainable<string>;
     mhGetRecipients(): Chainable<string[]>;
     mhHasMailWithSubject(subject : string): Chainable;
