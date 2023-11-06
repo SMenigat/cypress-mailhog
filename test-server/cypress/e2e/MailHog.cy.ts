6947d3b9dad50b878559797a87cd7431a7148316
Samuel Menigat <samuel.menigat@gmail.com>
2023-11-06 12:59:49 +0100

    feat(filters): adds mhFilterBySubject, mhFilterByRecipient, mhFilterBySender

--- test-server/cypress/e2e/MailHog.cy.ts
+++ test-server/cypress/e2e/MailHog.cy.ts
@@ -1,128 +1,139 @@
 const triggerAction = (actionName: string) => {
-  return cy
-    .get(`[data-action="${actionName}"`)
-    .click();
+  return cy.get(`[data-action="${actionName}"`).click();
 };
 
 const simulatedTransportDelay = 250;
 
-describe('MailHog', () => {
+describe("MailHog", () => {
   beforeEach(() => {
-    cy
-      .visit('/')
+    cy.visit("/")
       .mhDeleteAll()
       .mhSetJimMode(false);
   });
-  afterEach(() => {
-    cy.mhDeleteAll().mhSetJimMode(false);
+  it("loads the page", () => {
+    cy.get(".status-wrapper").should("be.visible");
   });
-  it('loads the page', () => {
-    cy.get('.status-wrapper').should('be.visible');
-  });
-  describe('Mail Collection', () => {
+  describe("Mail Collection", () => {
     beforeEach(() => {
       cy.wait(simulatedTransportDelay); // make sure to wait for delayed messages before cleaning up
       cy.mhDeleteAll();
-      triggerAction('generate-bulk');
+      triggerAction("generate-bulk");
     });
-    it('cy.mhGetAllMails() - returns an array containing all mails', () => {
-      cy.mhGetAllMails().should('have.length', 10);
+    it("cy.mhGetAllMails() - returns an array containing all mails", () => {
+      cy.mhGetAllMails().should("have.length", 10);
     });
-    it('cy.mhGetMailsBySubject(subject) - returns array of mails by subject', () => {
-      cy.mhGetMailsBySubject('Bulk Mail 1/10').should('have.length', 1);
+    it("cy.mhGetMailsBySubject(subject) - returns array of mails by subject", () => {
+      cy.mhGetMailsBySubject("Bulk Mail 1/10").should("have.length", 1);
     });
-    it('cy.mhGetMailsBySender(from) - returns array of mails by sender', () => {
-      cy.mhGetMailsBySender('single@example.com').should('have.length', 10);
+    it("cy.mhGetMailsBySender(from) - returns array of mails by sender", () => {
+      cy.mhGetMailsBySender("single@example.com").should("have.length", 10);
     });
-    it('cy.mhGetMailsByRecipient(recipient) - returns array of mails by recipient', () => {
-      cy
-        .mhGetMailsByRecipient('recipient@example.com')
-        .should('have.length', 10);
+    it("cy.mhGetMailsByRecipient(recipient) - returns array of mails by recipient", () => {
+      cy.mhGetMailsByRecipient("recipient@example.com").should(
+        "have.length",
+        10
+      );
     });
-    it('cy.mhDeleteAll() - delets all mails from MailCatcher', () => {
-      cy
-        .wait(simulatedTransportDelay) // make sure to wait for delayed messages before cleaning up
+    it("cy.mhDeleteAll() - delets all mails from MailCatcher", () => {
+      cy.wait(simulatedTransportDelay) // make sure to wait for delayed messages before cleaning up
         .mhDeleteAll()
         .mhGetAllMails()
-        .should('have.length', 0);
+        .should("have.length", 0);
     });
   });
-  describe('Handling a Single Mail ✉️', () => {
+  describe.only("Filter Mail Collection", () => {
     beforeEach(() => {
       cy.wait(simulatedTransportDelay); // make sure to wait for delayed messages before cleaning up
       cy.mhDeleteAll();
-      triggerAction('generate-single');
+      triggerAction("generate-bulk-unique");
     });
-    it('mail.mhGetSubject() - returns the mails subject', () => {
-      cy
+    it("cy.mhFilterBySubject(subject) - returns array of mails filtered by subject", () => {
+      cy.wait(1000)
         .mhGetAllMails()
-        .should('have.length', 1)
+        .mhFilterBySubject("Unique Mail 4/10")
+        .should("have.length", 1);
+    });
+    it("cy.mhGetMailsBySender(sender) - returns array of mails filtered by sender", () => {
+      cy.wait(1000)
+        .mhGetAllMails()
+        .mhFilterBySender("single-4@example.com")
+        .should("have.length", 1);
+    });
+    it("cy.mhGetMailsByRecipient(recipient) - returns array of mails filtered by recipient", () => {
+      cy.wait(1000)
+        .mhGetAllMails()
+        .mhFilterByRecipient("recipient-4@example.com")
+        .should("have.length", 1);
+    });
+  });
+  describe("Handling a Single Mail ✉️", () => {
+    beforeEach(() => {
+      cy.wait(simulatedTransportDelay); // make sure to wait for delayed messages before cleaning up
+      cy.mhDeleteAll();
+      triggerAction("generate-single");
+    });
+    it("mail.mhGetSubject() - returns the mails subject", () => {
+      cy.mhGetAllMails()
+        .should("have.length", 1)
         .mhFirst()
         .mhGetSubject()
-        .should('eq', 'Single Mail');
+        .should("eq", "Single Mail");
     });
-    it('mail.mhGetBody() - returns the message body', () => {
-      cy
-        .mhGetAllMails()
-        .should('have.length', 1)
+    it("mail.mhGetBody() - returns the message body", () => {
+      cy.mhGetAllMails()
+        .should("have.length", 1)
         .mhFirst()
         .mhGetBody()
-        .should('contain', 'HTML message body');
+        .should("contain", "HTML message body");
     });
-    it('mail.mhGetSender() - returns sender', () => {
-      cy
-        .mhGetAllMails()
-        .should('have.length', 1)
+    it("mail.mhGetSender() - returns sender", () => {
+      cy.mhGetAllMails()
+        .should("have.length", 1)
         .mhFirst()
         .mhGetSender()
-        .should('equal', 'single@example.com');
+        .should("equal", "single@example.com");
     });
-    it('mail.mhGetRecipients() - returns recipients', () => {
-      cy
-      .mhGetAllMails()
-      .should('have.length', 1)
-      .mhFirst()
-      .mhGetRecipients()
-      .should('contain', 'bcc-recipient@example.com');
+    it("mail.mhGetRecipients() - returns recipients", () => {
+      cy.mhGetAllMails()
+        .should("have.length", 1)
+        .mhFirst()
+        .mhGetRecipients()
+        .should("contain", "bcc-recipient@example.com");
     });
-    it('mail.mhHasAttachment() - asserts if mail has attachment');
+    it("mail.mhHasAttachment() - asserts if mail has attachment");
   });
-  describe('Asserting the Mail Collection 🔍', () => {
+  describe("Asserting the Mail Collection 🔍", () => {
     beforeEach(() => {
       cy.mhDeleteAll();
-      triggerAction('generate-single');
+      triggerAction("generate-single");
     });
-    it('cy.mhHasMailWithSubject(subject) - asserts for mail by subject', () => {
-      cy.mhHasMailWithSubject('Single Mail');
+    it("cy.mhHasMailWithSubject(subject) - asserts for mail by subject", () => {
+      cy.mhHasMailWithSubject("Single Mail");
     });
-    it('cy.mhHasMailFrom(from) - asserts for mail by sender', () => {
-      cy.mhHasMailFrom('single@example.com');
+    it("cy.mhHasMailFrom(from) - asserts for mail by sender", () => {
+      cy.mhHasMailFrom("single@example.com");
     });
-    it('cy.mhHasMailTo(recipient) - asserts for mail by recipient', () => {
-      cy
-        .mhHasMailTo('recipient@example.com')
-        .mhHasMailTo('cc-recipient@example.com')
-        .mhHasMailTo('bcc-recipient@example.com');
+    it("cy.mhHasMailTo(recipient) - asserts for mail by recipient", () => {
+      cy.mhHasMailTo("recipient@example.com")
+        .mhHasMailTo("cc-recipient@example.com")
+        .mhHasMailTo("bcc-recipient@example.com");
     });
   });
-  describe('Jim Chaos Monkey 🐵', () => {
-    it('cy.mhGetJimMode() - returns if jim mode is enabled', () => {
-      cy
-        .mhSetJimMode(true)
+  describe("Jim Chaos Monkey 🐵", () => {
+    it("cy.mhGetJimMode() - returns if jim mode is enabled", () => {
+      cy.mhSetJimMode(true)
         .mhGetJimMode()
-        .should('eq', true);
+        .should("eq", true);
     });
-    it('cy.mhSetJimMode(true) - enables jim mode', () => {
-      cy
-        .mhSetJimMode(true)
+    it("cy.mhSetJimMode(true) - enables jim mode", () => {
+      cy.mhSetJimMode(true)
         .mhGetJimMode()
-        .should('eq', true);
+        .should("eq", true);
     });
-    it('cy.mhSetJimMode(false) - disables jim mode', () => {
-      cy
-        .mhSetJimMode(false)
+    it("cy.mhSetJimMode(false) - disables jim mode", () => {
+      cy.mhSetJimMode(false)
         .mhGetJimMode()
-        .should('eq', false);
+        .should("eq", false);
     });
   });
 });
--- test-server/lib/Mails.php
+++ test-server/lib/Mails.php
@@ -1,65 +1,81 @@
 <?php
+
 namespace Lib;
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-class Mails {
-  
+class Mails
+{
+
   /**
    * Creates a mail object with basic connection settings set
+   * 
+   * @param string $from
+   * @param string $recipient
    *
    * @return PHPMailer
    */
-  private function createMail() {
-    $mail = new PHPMailer(true);   
+  private function createMail($from = 'single@example.com', $recipient = 'recipient@example.com')
+  {
+    $mail = new PHPMailer(true);
     $mail->SMTPDebug = 2;
     $mail->isSMTP();
-    $mail->Host = 'mailhog'; 
+    $mail->Host = 'mailhog';
     $mail->SMTPAuth = false;
     $mail->Port = 1025;
 
     // create with some basic metadata
-    $mail->setFrom('single@example.com', 'Single');
-    $mail->addAddress('recipient@example.com', 'Recipient');
+    $mail->setFrom($from, 'Single');
+    $mail->addAddress($recipient, 'Recipient');
     $mail->addCC('cc-recipient@example.com');
     $mail->addBCC('bcc-recipient@example.com');
 
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = $subject;
+    $mail->Subject = 'Demo Subject';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
 
-    return $mail;        
+    return $mail;
   }
 
   /**
    * Sends a single name with given subject
    *
    * @param string $subject
+   * @param string $from
+   * @param string $recipient
    * @return void
    */
-  public function sendSingle($subject = 'Single Mail') {   
-    $mail = $this->createMail();                              // Set email format to HTML
+  public function sendSingle($subject = 'Single Mail', $from = 'single@example.com', $recipient = 'recipient@example.com')
+  {
+    $mail = $this->createMail($from, $recipient);                              // Set email format to HTML
     $mail->Subject = $subject;
     $mail->send();
   }
 
-  public function sendSingleWithAttachment() {
+  public function sendSingleWithAttachment()
+  {
     $mail = $this->createMail();
     $mail->Subject = 'Mail with Attachment';
-    $mail->addAttachment(__DIR__.'/../attachment.txt');
+    $mail->addAttachment(__DIR__ . '/../attachment.txt');
     $mail->send();
   }
 
   /**
-   * Sends given ammount of mails
+   * Sends given amount of mails
    *
-   * @param integer $ammount
+   * @param integer $amount
+   * @param boolean $uniqueSenders
    * @return void
    */
-  public function sendBulk($ammount = 10) {
-    for($i = 1; $i <= $ammount; $i++) {
-      $this->sendSingle("Bulk Mail ${i}/${ammount}");
+  public function sendBulk($amount = 10, $uniqueSenders = false)
+  {
+    for ($i = 1; $i <= $amount; $i++) {
+      if ($uniqueSenders) {
+        $this->sendSingle("Unique Mail ${i}/${amount}", "single-${i}@example.com", "recipient-${i}@example.com");
+      } else {
+        $this->sendSingle("Bulk Mail ${i}/${amount}");
+      }
     }
   }
 }
